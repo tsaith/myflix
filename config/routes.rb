d1630448393143f0b2446565d7aeab26086ff8ec
Tsung-Hua Tsai <tsai.tsunghua@gmail.com>
2014-12-02 18:19:16 +0800

    Show the recent videos at the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Myflix::Application.routes.draw do
-  root to: 'videos#index'
+  root to: 'categories#index'
+
   get 'ui(/:action)', controller: 'ui'
   get 'todo(/:action)', controller: 'todos'
   resources :videos, only: [:show] do
