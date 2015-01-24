868072901eeda3acb78c2d1275692cf6a231a11c
Tsung-Hua Tsai <tsai.tsunghua@gmail.com>
2015-01-24 11:57:23 +0800

    Set AWS S3 id and key.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,15 @@ CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.storage = :fog
     config.fog_credentials = {
-      :provider               => 'AWS',       # required
-      :aws_access_key_id      => '',       # required
-      :aws_secret_access_key  => '', # required
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
     }
-    config.fog_directory  = 'myflix-tokyo' # bucket name
+    if Rails.env.staging?
+      config.fog_directory  = 'myflix-staging' # bucket name
+    else
+      config.fog_directory  = 'myflix-production' # bucket name
+    end
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
